eeed3c565c615f7d80c5f1166101d3a1885cc65d
Ayelen may <Ayelen.kirchhoff@gmail.com>
2025-06-12 13:28:43 +0200

    defined Animal_web_generator

---
+++ Animal_web_generator.py
@@ -0,0 +1,59 @@
+""" reads the content of animals_data.json,
+iterates through the animals,and for each one prints:
+Name
+Diet
+The first location from the locations list
+Type"""
+
+
+import json
+import os
+
+"""Load an return json file"""
+def load_data(file_path):
+    with open(file_path, "r") as file:
+        return json.load(file)
+
+file_name = "animals_data.json"
+
+
+#path to data file
+if os.path.exists(file_name):
+    data = load_data(file_name)
+
+    for animal in data:
+        name = animal.get("name")
+        diet = animal.get("diet")
+        location = animal.get("locations", [])
+        animal_type = animal.get("type")
+
+
+        print(f"Name: {name}")
+        print(f"Diet: {diet}")
+        print(f"first known location: {location [0] if location else 'unknown'}")
+        print(f"Type: {animal_type}")
+        print("-" * 30)
+else:
+    print(f"File '{file_name}' does not exist.")
+
+#file exists ?
+if os.path.exists(file_name):
+    #open and load
+    with open(file_name, 'r') as file:
+        data = json.load(file)
+
+    for animal in data:
+        name = animal.get("name")
+        diet = animal.get("diet")
+        location = animal.get("locations", [])
+        animal_type = animal.get("type")
+
+
+        print(f"Name: {name}")
+        print(f"Diet: {diet}")
+
+        print(f"first known location: {location [0] if location else 'unknown'}")
+        print(f"Type: {animal_type}")
+        print("-" * 30)
+else:
+    print(f"File '{file_name}' does not exist.")
